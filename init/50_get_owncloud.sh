2e66466239a0aebdc5f580c7580c11782438a5fe
Alex Wood <github@alex-wood.org.uk>
2016-10-31 23:32:12 +0000

    Check for (and install if needed) new versions of OwnCloud every (re)start.

--- init/50_get_owncloud.sh
+++ init/50_get_owncloud.sh
@@ -6,6 +6,11 @@ owncloud_vers=$(curl -L http://tools.linuxserver.io/lsio/owncloud.php | jq .pack
 curl -o /tmp/owncloud.tar.bz2 -L "$owncloud_vers"
 mkdir -p /config/www/owncloud
 tar -xjf /tmp/owncloud.tar.bz2 -C /config/www/owncloud  --strip-components=1
+else
+    #see if and update is available, as already installed
+    cd /config/www/owncloud
+    sudo -u abc php occ upgrade
+    cd -
 fi
 
 chown -R abc:abc /config/www/owncloud
